14bd34f3bb2ccbb675451ab3a19914f1d15899c2
YChao <yc12877782@gmail.com>
2019-02-01 17:10:43 +0800

    test

---
+++ pool.js
@@ -0,0 +1,20 @@
+/*
+*    MYSQL数据库连接池
+*/ 
+
+const mysql = require('mysql');
+var pool = mysql.createPool({
+	/*
+    host: '127.0.0.1', //数据库地址
+    port: 3306,         //数据库端口
+    user: 'root',       //数据库管理员
+    database: 'xiaofeiniu', //默认连接的数据库
+    connectionLimit: 10      //连接池中连接数量*/
+	host     : process.env.MYSQL_HOST,
+    port     : process.env.MYSQL_PORT,
+    user     : process.env.ACCESSKEY,
+    password : process.env.SECRETKEY,
+    database : 'app_' + process.env.app_xiaofeiniu
+	connectionLimit: 3
+});
+module.exports = pool;
